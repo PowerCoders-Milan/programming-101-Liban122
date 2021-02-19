3d6696c09e6ece66cde57cfc3210a8b3e25d008e
liban <botanlee515@gmail.com>
2021-02-19 13:17:36 +0100

    added

--- 01-Blockly/blockly.js
+++ 01-Blockly/blockly.js
@@ -1,1 +1,25 @@
 //Insert you code below
+
+//No. 9
+while (notDone()) {
+    if (isPathLeft()) {
+      turnLeft();
+    }
+    if (isPathForward()) {
+      moveForward();
+    } else {
+      turnRight();
+    }
+  }
+
+  // No. 10
+  while (notDone()) {
+    if (isPathLeft()) {
+      turnLeft();
+    }
+    if (isPathForward()) {
+      moveForward();
+    } else {
+      turnRight();
+    }
+  }
--- 02-Sorting-Numbers/sorting_numbers.js
+++ 02-Sorting-Numbers/sorting_numbers.js
@@ -1,6 +1,9 @@
 //insert your pseudocode below
 /*
-here
+1. Create a list of numbers
+2. Assign the list to a variable
+3. make the list in oreder decending/ascending "Action"
+4. Print the result
 */
 
 //insert your code below
