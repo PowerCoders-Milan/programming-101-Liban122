398c879d4ce27865d83f3bd8593dc0df4e5c8031
liban <botanlee515@gmail.com>
2021-02-24 13:58:22 +0100

    Done

--- 02-Sorting-Numbers/sorting_numbers.js
+++ 02-Sorting-Numbers/sorting_numbers.js
@@ -7,3 +7,21 @@
 */
 
 //insert your code below
+var digits;
+
+function listsGetSortCompare(type, direction) {
+  var compareFuncs = {
+    "NUMERIC": function(a, b) {
+        return Number(a) - Number(b); },
+    "TEXT": function(a, b) {
+        return a.toString() > b.toString() ? 1 : -1; },
+    "IGNORE_CASE": function(a, b) {
+        return a.toString().toLowerCase() > b.toString().toLowerCase() ? 1 : -1; },
+  };
+  var compare = compareFuncs[type];
+  return function(a, b) { return compare(a, b) * direction; }
+}
+
+
+digits = [5, 8, 2];
+window.alert(digits.slice().sort(listsGetSortCompare("NUMERIC", 1)));
